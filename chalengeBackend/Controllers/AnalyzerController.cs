550d90434f516f0e4a1dccf61e7ea0e918455744
hamedTir <tirandazhamed71@gmail.com>
2023-09-20 20:17:40 +0330

    Analyzer added

---
+++ chalengeBackend/Controllers/AnalyzerController.cs
@@ -0,0 +1,29 @@
+﻿namespace ChalengeBackend.Controllers
+{
+    using ChalengeBackend.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/analyzer")]
+    [ApiController]
+    public class AnalyzerController : ControllerBase
+    {
+        [HttpPost("reverseNum")]
+        public IActionResult ReverseNum([FromBody] uint number)
+        {
+            try
+            {
+                if (AnalyzerService.TryReverse(number, out uint reversedNumber))
+                {
+                    return Ok(reversedNumber);
+                }
+
+                return BadRequest("Invalid input");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+
+}
---
+++ chalengeBackend/Services/AnalyzerService.cs
@@ -0,0 +1,30 @@
+﻿namespace ChalengeBackend.Services
+{
+
+    using System;
+
+    public class AnalyzerService
+    {
+        public static bool TryReverse(uint number, out uint reversedNumber)
+        {
+            reversedNumber = 0;
+
+            // Check if the input number is a valid unsigned integer
+            if (number <= uint.MaxValue)
+            {
+                // Convert the number to a string and reverse it
+                char[] charArray = number.ToString().ToCharArray();
+                Array.Reverse(charArray);
+                string reversedNumberString = new string(charArray);
+
+                // Convert the reversed string back to an unsigned integer
+                if (uint.TryParse(reversedNumberString, out reversedNumber))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}

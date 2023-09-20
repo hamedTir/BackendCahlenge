5eac7791d8796f01f28f94efcf9d55072b66d2db
hamedTir <tirandazhamed71@gmail.com>
2023-09-20 20:54:42 +0330

    xunit test added

---
+++ TestProject/AnalyzerTest.cs
@@ -0,0 +1,37 @@
+﻿namespace TestProject
+{
+
+    using ChalengeBackend.Services;
+    using Xunit;
+
+    public class AnalyzerTest
+    {
+        [Fact]
+        public void TryReverse_ValidInput_ShouldSucceed()
+        {
+            // Arrange
+            uint input = 12345;
+            uint expectedReversedNumber = 54321;
+
+            // Act
+            bool result = AnalyzerService.TryReverse(input, out uint reversedNumber);
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(expectedReversedNumber, reversedNumber);
+        }
+
+        [Fact]
+        public void TryReverse_InvalidInput_ShouldFail()
+        {
+            // Arrange
+            uint input = uint.MaxValue; // This input is too large and will fail
+
+            // Act
+            bool result = AnalyzerService.TryReverse(input, out uint reversedNumber);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}

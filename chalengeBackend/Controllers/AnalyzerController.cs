5bf97a77d8a6ffd3f70f2442e32457ca75a10b75
hamedTir <tirandazhamed71@gmail.com>
2023-09-23 18:45:50 +0330

    longest substring api added

--- chalengeBackend/Controllers/AnalyzerController.cs
+++ chalengeBackend/Controllers/AnalyzerController.cs
@@ -24,6 +24,45 @@
                 return StatusCode(500, ex.Message);
             }
         }
-    }
 
+        [HttpPost("getLongestSubstring")]
+        public IActionResult GetLongestSubstring([FromBody] string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return BadRequest("Input string cannot be empty.");
+            }
+
+            int maxLength = 0;
+            int startIndex = 0;
+            int currentStart = 0;
+            int[] lastIndex = new int[256]; // Assuming ASCII characters
+
+            for (int i = 0; i < lastIndex.Length; i++)
+            {
+                lastIndex[i] = -1;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (lastIndex[c] >= currentStart)
+                {
+                    currentStart = lastIndex[c] + 1;
+                }
+
+                lastIndex[c] = i;
+
+                if (i - currentStart + 1 > maxLength)
+                {
+                    maxLength = i - currentStart + 1;
+                    startIndex = currentStart;
+                }
+            }
+
+            string longestSubstring = input.Substring(startIndex, maxLength);
+            return Ok(longestSubstring);
+        }
+    }
 }
+
